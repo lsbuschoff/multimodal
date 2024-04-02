cd898d605c74db08ce45993af245df1e5d7730e3
lsbuschoff <62058113+lsbuschoff@users.noreply.github.com>
2024-04-02 11:38:59 +0200

    Update main_adapter.py

--- eval/main_adapter.py
+++ eval/main_adapter.py
@@ -108,22 +108,13 @@ for epoch in tqdm(range(args.epochs)):
 
                     # Set prompts for relevant experiment
                     if exp == 0:
-                        instruction = "Q: What is the background color?" # You are only allowed to answer with a number corresponding to the correct background color. No words are allowed!
-                        # options = ["White", "Black", "Red", "Blue", "Yellow", "Green"]
-                        # instruction += helpers.configure_options(options, with_a=False)
-                    
+                        instruction = "Q: Consider the following scenario: a friend with a poor sense of balance has built a sculpture using a computer program. He asks you to help him decide whether it will fall, and if so, which way. Please respond with the background color!"
                     elif exp == 1:
-                        # instruction = "Q: How many blocks are in the image? You are only allowed to answer with the letter corresponding to the correct number of blocks. No words are allowed!"
-                        # options = ["1", "2", "3", "4", "5"]
-                        # instruction += helpers.configure_options(options, letters=True, with_a=False)
-                        instruction = "Q: How many blocks are in the image?" # You are only allowed to answer with a number between 1 and 5 corresponding to the correct number of blocks. No words are allowed!
-                        
+                        instruction = "Q: Consider the following scenario: a friend with a poor sense of balance has built a sculpture using a computer program. He asks you to help him decide whether it will fall, and if so, which way. Please respond with the number of blocks in the image!"
                     elif exp == 2:
-                        instruction = "Q: Is the beam balanced? If not, answer whether it will tip left or tip right."
-
+                        instruction = "Q: Consider the following scenario: a friend with a poor sense of balance has built a sculpture using a computer program. He asks you to help him decide whether it will fall, and if so, which way. Please respond by answering whether the beam would “tip left”, “stay balanced”, or “tip right”!"  
                     # Load image
                     prompts += (f'\nEXP_{exp+1}, {seq}: ')
-                    # url = os.path.join(directory, seq) + "/frame_0.png"
                     img = Image.open(seq)
                     img = preprocess(img).unsqueeze(0).to(device)
 
@@ -186,22 +177,46 @@ for epoch in tqdm(range(args.epochs)):
 
                     # Set prompts for relevant experiment
                     if exp == 0:
-                        instruction = "Q: The scene shows two balls labeled A and B. On the left side there is a pink gate. The solid arrows show the trajectories of the balls. What is the background color?" # You are only allowed to answer with a number corresponding to the correct background color. No words are allowed!
-                        # options = ["White", "Black", "Red", "Blue", "Yellow", "Green"]
-                        # instruction += helpers.configure_options(options, with_a=False)
-                    
+                        instruction = "Q: The scene shows two balls labeled “A”, and “B”. On the left side there is a pink gate. The solid arrows show the trajectories of the balls. What is the background color?" 
                     elif exp == 1:
-                        instruction = "Q: The scene shows two balls labeled A and B. On the left side there is a pink gate. The solid arrows show the trajectories of the balls. Does ball B enter the gate after it has been hit by ball?" # You are only allowed to answer with a number corresponding to the correct background color. No words are allowed!
-                        # options = ["1", "2", "3", "4", "5"]
-                        # instruction += helpers.configure_options(options, letters=True, with_a=False)
-                        instruction = "Q: Does Ball B ?" # You are only allowed to answer with a number between 1 and 5 corresponding to the correct number of blocks. No words are allowed!
-                        
+                        instruction = "Q: The scene shows two balls labeled “A”, and “B”. On the left side there is a pink gate. The solid arrows show the trajectories of the balls. Does ball B enter the gate after it has been hit by ball?"
                     elif exp == 2:
-                        instruction = "Q: The scene shows two balls labeled A and B. On the left side there is a pink gate. The solid arrows show the trajectories of the balls. Would ball B have entered the gate if it had not been hit by ball A?" # You are only allowed to answer with a number corresponding to the correct background color. No words are allowed!
+                        instruction = "Q: The scene shows two balls labeled “A”, and “B”. On the left side there is a pink gate. The solid arrows show the trajectories of the balls. Would ball B have entered the gate if it had not been hit by ball A?" 
                         
                     # Load image
                     prompts += (f'\nEXP_{exp+1}, {seq}: ')
-                    # url = os.path.join(directory, seq) + "/frame_0.png"
+                    img = Image.open(seq)
+                    img = preprocess(img).unsqueeze(0).to(device)
+
+                    # Pass input to model
+                    prompt = llama.format_prompt(instruction)
+                    response = model.generate(img, [prompt])[0]
+                    prompts += (f'{response}\n')
+
+        # For Balance stimuli
+        if args.dataset == "BTOM":
+
+             # Set directory and get list of all images
+            directory = "images/btom_stimuli"
+            images = glob.glob(f'{directory}/*.png')
+            images = natsorted(images)
+
+            # Loop through experiments
+            for exp in range(3):
+
+                # Loop through images
+                for i, seq in enumerate(images):
+
+                    # Set prompts for relevant experiment
+                    if exp == 0:
+                        instruction = "Q: Consider the following scenario: a hungry student leaves his office looking for lunch from one of three food trucks: Korean, Lebanese or Mexican. The university provides only two parking spots, so at most two trucks can be on campus on any given day. The student is marked by a triangle, the three trucks are marked by letters (K, Korean; L, Lebanese; M, Mexican), parking spaces are marked by yellow regions, and buildings (which block movement and line of sight visibility) are marked by black rectangles. In the image you see the trajectory of the student as he is looking for lunch. What is the background color?"
+                    elif exp == 1:
+                        instruction = "Q: Consider the following scenario: a hungry student leaves his office looking for lunch from one of three food trucks: Korean, Lebanese or Mexican. The university provides only two parking spots, so at most two trucks can be on campus on any given day. The student is marked by a triangle, the three trucks are marked by letters (K, Korean; L, Lebanese; M, Mexican), parking spaces are marked by yellow regions, and buildings (which block movement and line of sight visibility) are marked by black rectangles. In the image you see the trajectory of the student as he is looking for lunch. How many food trucks are in the image?"
+                    elif exp == 2:
+                        instruction = "Q: Consider the following scenario: a hungry student leaves his office looking for lunch from one of three food trucks: Korean, Lebanese or Mexican. The university provides only two parking spots, so at most two trucks can be on campus on any given day. The student is marked by a triangle, the three trucks are marked by letters (K, Korean; L, Lebanese; M, Mexican), parking spaces are marked by yellow regions, and buildings (which block movement and line of sight visibility) are marked by black rectangles. In the image you see the trajectory of the student as he is looking for lunch. What is his favorite food truck, and why?"
+                    
+                    # Load image
+                    prompts += (f'\nEXP_{exp+1}, {seq}: ')
                     img = Image.open(seq)
                     img = preprocess(img).unsqueeze(0).to(device)
 
